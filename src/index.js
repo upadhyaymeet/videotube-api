67bd13a4ca81e289029b43c864d7b2d2e26930ce
Meet <codewithmeet25@gmail.com>
2024-04-08 15:01:09 +0530

    Express app configure

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+import dotenv from 'dotenv'
 import connectToDB from "./db/index.js";
+import { app } from './app.js';
+
+dotenv.config(
+    {
+        path:"./.env"
+    }
+)
 
 connectToDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`App listening on http://localhost:${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log("Mongodb db connection Failed", error);
+})
