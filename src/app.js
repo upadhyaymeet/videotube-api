42857bd7b5f2e7a35972979edb3a8156fdfce9d3
Meet <codewithmeet25@gmail.com>
2024-04-08 17:41:29 +0530

    user routes setup

--- src/app.js
+++ src/app.js
@@ -15,4 +15,8 @@ app.use(express.static("public"))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(cookieParser())
 
+import userRouter from "./routes/user.routes.js"
+
+app.use("/api/v1/user", userRouter)
+
 export {app}
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,12 @@
+import { asyncHandler } from "../utils/AsyncHandler.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+
+const registerUser = asyncHandler(async(req, res)=>{
+    
+})
+
+export {
+    registerUser
+}
