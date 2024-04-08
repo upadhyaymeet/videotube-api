4755c9bbbc75742fad88cc8831441c39da2ca525
Meet <codewithmeet25@gmail.com>
2024-04-08 15:23:01 +0530

    express app configured

--- src/app.js
+++ src/app.js
@@ -1,6 +1,18 @@
+import cookieParser from 'cookie-parser'
 import express from 'express'
 
 const app = express()
 
+app.use(cors(
+    {
+        origin:process.env.CORS_ORIGIN,
+        Credentials:true
+    }
+))
+
+app.use(express.json({limit:"16kb"}))
+app.use(express.static("public"))
+app.use(express.urlencoded({extended:true, limit:"16kb"}))
+app.use(cookieParser())
 
 export {app}
