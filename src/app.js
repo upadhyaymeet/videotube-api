2953362bee1659c8a3a08d1e34c96f82a8d4cb03
Meet <codewithmeet25@gmail.com>
2024-04-13 14:49:03 +0530

    dashboard router and controller

--- src/app.js
+++ src/app.js
@@ -22,6 +22,8 @@ import commentRouter from "./routes/comment.routes.js"
 import likeRouter from "./routes/like.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
 import healthCheckRouter from "./routes/healthCheck.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
 
 app.use("/api/v1/user", userRouter)
 app.use("/api/v1/video", videoRouter)
@@ -30,6 +32,8 @@ app.use("/api/v1/comment", commentRouter)
 app.use("/api/v1/like", likeRouter)
 app.use("/api/v1/tweet",tweetRouter)
 app.use("/api/v1/healthcheck", healthCheckRouter)
+app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 
 
 export {app}
